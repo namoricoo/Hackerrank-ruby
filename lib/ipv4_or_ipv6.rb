dc757d9f87e7a6b98222d8f4adb23b101bb1aba8
Nate <namoricoo@hotmail.com>
2014-01-10 22:41:59 -0800

    pass IPv4, IPv6, and neither test

--- lib/ipv4_or_ipv6.rb
+++ lib/ipv4_or_ipv6.rb
@@ -1,7 +1,13 @@
 def ipv4_or_ipv6(input_string) 
- if input_string =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+ if input_string =~ /[\d{1,3}\.]{3}[\d{1,3}]/
    'IPv4'
+ elsif ipv6_address?(input_string)
+   'IPv6'
  else
    'Neither'  
  end
+end
+
+def ipv6_address?(input)
+  input =~ /[[0-9a-fA-F]{0,4}\:]{5,7}[0-9a-fA-F]{0,4}/
 end
--- spec/ipv4_or_ipv6_spec.rb
+++ spec/ipv4_or_ipv6_spec.rb
@@ -6,4 +6,9 @@ describe 'ipv4_or_ipv6' do
     output = 'IPv4'
     ipv4_or_ipv6(input).should == output
   end
+  it 'return IPV6 if an IPV6 I.P, address detected' do
+    input = 'fe80:fe80:200:5aee:feaa:20a2'
+    output = 'IPv6'
+    ipv4_or_ipv6(input).should == output
+  end
 end
