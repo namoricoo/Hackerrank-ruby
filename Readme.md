35611c5e8be3b0a924a4158cedbb8efb4e8fa637
Nate <namoricoo@hotmail.com>
2014-01-10 19:20:44 -0800

    initial commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Hackerrank - Ruby - Rspec
+=========================
---
+++ spec/ipv4_or_ipv6_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require_relative '../lib/ipv4_or_ipv6.rb'
+describe 'ipv4_or_ipv6' do
+  it "return IPV4 if an IPV4 I.P, address detected" do
+    input = '127.255.255.255'
+    output = 'IPv4'
+    ipv4_or_ipv6(input).should == output
+  end
+end
